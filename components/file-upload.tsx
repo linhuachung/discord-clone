53651ab2872eb81510d8266e35e16b6fe5c5e682
Chun <chunglh@bap.jp>
2023-12-08 14:18:51 +0700

    [IOT][INTERATE-API][TENANT-MANAGERMANT] update set active tenant

---
+++ components/file-upload.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react';
+
+const FileUpload = (props) => {
+    return (
+        <div>sdfsdf</div>
+    )
+};
+
+export default FileUpload;
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -15,6 +15,7 @@ import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {zodResolver} from "@hookform/resolvers/zod";
+import FileUpload from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -60,7 +61,17 @@ const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                Image upload
+                                <FormField
+                                    control={form.control}
+                                    name={'imageUrl'}
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload/>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField
                                 control={form.control}
